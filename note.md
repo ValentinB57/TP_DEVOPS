81da1094c76c5ebbbf3b16677ae2e6bf28e250fd
Dylanabo <dylan.abodi@epitech.eu>
2023-01-26 18:06:13 +0400

    documentation

--- note.md
+++ note.md
@@ -1,2 +1,7 @@
+# Crée une image Docker à partir du Dockerfile
 docker build -t node-app:latest .
+
+# Démarre un conteneur à partir de l'image créée précédemment
+# -d : permet de démarrer le conteneur en arrière-plan
+# -p : permet de publier les ports exposés par le conteneur pour les rendre accessibles depuis l'hôte. ici on expose le port 3000 de l'hôte avec le port 3000 du conteneur
 docker run -d -p 3000:3000 node-app:latest
--- src/app.test.js
+++ src/app.test.js
@@ -1,11 +1,22 @@
+// Importe l'instance de l'application
 const app = require("./app")
+
+// Importe les modules supertest et jest
 const supertest = require("supertest")
+const { expect } = require("jest")
+
+// Crée une instance de supertest pour tester l'application
 const request = supertest(app)
 
 describe("/test endpoint", () => {
     it("should return a response", async () => {
+        // Envoie une requête GET à l'endpoint '/test'
         const response = await request.get("/test")
+
+        // Vérifie que la réponse a un statut 200 (OK)
         expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
+
+        // Vérifie que le corps de la réponse est égal à "Hello world"
+        expect(response.text).toBe("Hello world")
     })
 })
