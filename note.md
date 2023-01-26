75cbbafcefdfa8f244dbf07d6e0049f9461364e1
Dylanabo <dylan.abodi@epitech.eu>
2023-01-26 22:24:20 +0400

    page

--- note.md
+++ note.md
@@ -5,3 +5,10 @@ docker build -t node-app:latest .
 # -d : permet de démarrer le conteneur en arrière-plan
 # -p : permet de publier les ports exposés par le conteneur pour les rendre accessibles depuis l'hôte. ici on expose le port 3000 de l'hôte avec le port 3000 du conteneur
 docker run -d -p 3000:3000 node-app:latest
+
+
+
+#deploiement
+
+
+https://app.netlify.com/sites/zesty-fudge-22a680/settings/deploys
--- src/app.js
+++ src/app.js
@@ -5,8 +5,12 @@
 // Importe le module express
 const express = require("express");
 
+// Importe le module path
+const path = require('path');
+
 // Crée une instance de l'application express
 const app = express();
+
 
 // Indique à l'application d'utiliser le répertoire 'public' pour servir les fichiers statiques
 app.use(express.static('public'));
@@ -16,6 +20,13 @@ app.get("/test", (_req, res) =>  {
   // Envoie une réponse HTTP avec un statut 200 et un message "Hello world"
   res.status(200).send("Hello world")
 })
+
+// Ajoute une page de base  pour les toutes les routes non définies'
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/minecraft.html'));
+});
+
 
 // Exporte l'instance de l'application pour pouvoir l'utiliser dans d'autres fichiers
 module.exports = app;
